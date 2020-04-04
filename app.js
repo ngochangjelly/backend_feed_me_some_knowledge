dd4f043ffc4fa47c22c1971a7d1057b3e4c1b4de
thminhVN <thminhkg@gmail.com>
2020-04-05 00:02:21 +0700

    minor fix

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+  res.send('An alligator approaches!');
 });
 
-app.listen(process.env.APP_URL, () => console.log('Gator app listening on port!'));
+app.get('/hello', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(process.env.APP_URL, () => {
+  console.log('Gator app listening on port!');
+});

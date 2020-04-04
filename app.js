df9a64ab333b8c3e993817689db60f33b1b48a5f
thminhVN <thminhkg@gmail.com>
2020-04-04 23:57:09 +0700

    change env port

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 
-app.listen(process.env.DATABASE_URL, () => console.log('Gator app listening on port!'));
+app.listen(process.env.APP_URL, () => console.log('Gator app listening on port!'));
